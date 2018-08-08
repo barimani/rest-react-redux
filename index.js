47fe701ef428a3599a7b552dfe1adc1a5d28c001
unknown <moh.barimani@gmail.com>
2018-08-07 20:15:38 -0700

    Object define for exporting detailedEntity

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ Object.defineProperty(exports , 'queriedEntityReducer', {
 Object.defineProperty(exports , 'queriedEntity', {
     value: lib.queriedEntity
 });
+
+Object.defineProperty(exports , 'detailedEntityReducer', {
+    value: lib.detailedEntityReducer
+});
+
+Object.defineProperty(exports , 'detailedEntity', {
+    value: lib.detailedEntity
+});
