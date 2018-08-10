af42413ce34b4b24aebb0c7d188d960fd14e6e6a
Bari <bari.barimani@twotalltotems.com>
2018-08-10 16:34:56 -0700

    First simple sanity test with chai and mocha

---
+++ Tests/test.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+const expect = require('chai').expect;
+// import { shallow } from 'enzyme';
+
+// const Component = () => <div>hi</div>;
+
+describe('Sanity', () => {
+    it('1 and 1 are equal', () => {
+            expect(1).to.equal(1);
+        }
+    );
+});
