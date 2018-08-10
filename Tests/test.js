0e00d9abefd1c54d8a10b2b6def24dbb99c2a0f6
Bari <bari.barimani@twotalltotems.com>
2018-08-10 16:58:05 -0700

    Ability to test a dummy react component and shallow testing with Enzyme

--- Tests/test.js
+++ Tests/test.js
@@ -1,12 +1,19 @@
-// import React from 'react';
+import React from 'react';
 import {expect} from 'chai';
-// import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-// const Component = () => <div>hi</div>;
+Enzyme.configure({ adapter: new Adapter() });
+
+const Component = () => <div>hi</div>;
 
 describe('Sanity', () => {
-    it('1 and 1 are equal', () => {
-            expect(1).to.equal(1);
-        }
-    );
+    it('checks 1 and 1 are equal', () => {
+        expect(1).to.equal(1);
+    });
+
+    it('checks whether Component can be rendered', () => {
+        const wrapper = shallow(<Component/>);
+        expect(wrapper.find('div')).to.have.length(1);
+    });
 });
